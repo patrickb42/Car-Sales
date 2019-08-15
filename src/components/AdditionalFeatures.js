a6a09137b49a32c8e0a98edfe7d419a14315f092
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-14 22:28:27 -0400

    deconstructed props

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 
-const AdditionalFeatures = props => {
+const AdditionalFeatures = ({ store }) => {
   return (
     <div className="content">
       <h4>Additional Features</h4>
-      {props.store.length ? (
+      {store.length ? (
         <ol type="1">
-          {props.store.map(item => (
+          {store.map(item => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
         </ol>
